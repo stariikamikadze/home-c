355e22fa0849a89929f7bfede77934df7a066699
stariikamikadze <91565147+stariikamikadze@users.noreply.github.com>
2025-04-20 17:26:00 +0300

    домашняя работа
    домашняя работа

---
+++ christmastree.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+/*
+       *
+      ***
+     *****
+    *******
+     HH HH
+     ZZZZZ
+
+*/
+		printf("       *\n");
+		printf("      ***\n");
+		printf("     *****\n");
+		printf("    *******\n");
+		printf("     HH HH\n");
+		printf("     ZZZZZ\n");
+		return 0;
+}
---
+++ ladder.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	
+/*
+Let's
+	 Go
+		To
+		   Walk
+*/
+	printf("Let's\n");
+	printf("	 Go\n");
+	printf("		To\n");
+	printf("		   Walk\n");
+		return 0; 
+
+}
